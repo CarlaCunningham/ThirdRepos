67d36085e20a81a85a00f181c92e1c72966e4ee7
Carla Cunningham <carlacunningham@student.tafesa.edu.au>
2024-09-12 00:29:50 +0930

    commit the child discount changes

--- MyInitial/TicketsForm.cs
+++ MyInitial/TicketsForm.cs
@@ -32,6 +32,8 @@ namespace Ticketing
 
             if (chkDiscount.Checked)
                 { mDiscount = true; }
+            if (chkChild.Checked)
+                { mDiscount = true; }
 
             if (radBalcony.Checked)
                 { mSection = 1; }
